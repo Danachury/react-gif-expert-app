f3ae4690927eeb2112608c1ee70dd9dd1892767a
Denilson Anachury <danachuryc@gmail.com>
2023-08-30 16:28:40 -0500

    :construction: feat: add configuration to mock `styles` on unit tests and `fetch api`

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,4 +1,10 @@
 module.exports = {
     testEnvironment: 'jest-environment-jsdom',
-    setupFiles: ['./jest.setup.js']
+    setupFiles: ['./jest.setup.js'],
+    moduleNameMapper: {
+        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+            '<rootDir>/__tests/__mocks__/fileMock.js',
+        '\\.(css|less)$': '<rootDir>/__tests__/__mocks__/styleMock.js',
+    },
+    testPathIgnorePatterns: ['<rootDir>/__tests__/__mocks__/']
 }
